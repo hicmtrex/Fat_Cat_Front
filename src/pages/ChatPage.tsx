37ffc266810bc74e7e29e38664969ee9c3dbb2cd
Firstname Lastname <your_email@youremail.com>
2022-10-07 04:13:29 +0100

    added

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -4,11 +4,17 @@ import SlideDrawer from '../components/Layouts/SlideDrawer';
 import MyChats from '../components/Chats/MyChats';
 import ChatBox from '../components/Chats/ChatBox';
 import useAuthStore from '../store/useAuth';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ChatPage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { user } = useAuthStore((state) => state);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!user) navigate('/auth');
+  }, [user]);
   return (
     <div style={{ width: '100%' }}>
       <Navbar onOpen={onOpen} />
